1447e8681417a180274f5933652f2854046bd430
brunocesarangst <bruno.angst@rede.ulbra.br>
2020-02-11 10:56:15 -0300

    feat: iniciando useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // com useState criamos o estado que pertence a função.
 // com useEffect podemos sobrepor os métodos do ciclo de vida
 
 function App() {
   // para cada tipo de informação dentro do componente nos teremos um state
   // separado.
-  const [techs, setTechs] = useState(['ReactJS', 'React-Native']);
+  const [techs, setTechs] = useState([]);
   // armazenando o state de tecnologias
   // useState retorna uma array na 1ª posição o state(a lista de tecnologia) a
   // 2ª posição é a atualização do state.
@@ -23,6 +23,28 @@ function App() {
     setNewTech('');
     // aqui "resetamos" o input
   }
+
+  // useEffect para ciclo de vida
+  // executando uma única vez, para montar o componente em tela.
+  useEffect(() => {
+    // buscado s techs do local storage
+    const techsInStorage = localStorage.getItem('techs');
+
+    // se tem informações
+    if (techsInStorage) {
+      setTechs(JSON.parse(techsInStorage));
+    }
+    // aqui não em varável para monitorar vai executar uma única vez
+  }, []);
+
+  // armazenando as techs no local Storage.
+  // o 1º parâmetro é a função que vai ser executada e 2º é quando ela vai ser
+  // executada que é um array de dependências que monitora alterações em certas
+  // variáveis
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+    // toda vez que o valor de techs for alterado.
+  }, [techs]);
 
   return (
     <>
